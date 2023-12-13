eac630db33ad81fbaaeb5d8fe16c180817af8109
linnjoha <linn.wallin@hotmail.com>
2023-12-13 21:19:35 +0100

    final

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,19 +1,11 @@
-const tip = parseFloat(document.getElementById("tip").value);
-const sum = parseFloat(document.getElementById("sum").value);
-const numberOfFriends = parseFloat(document.getElementById("numberOfFriends").value);
 function calculateTip(sum, tip) {
     const total = sum * tip;
     return total;
 }
-//calculateTip(sum, tip);
-const calculatedTip = calculateTip(sum, tip);
-const total = sum + calculatedTip;
 function divideTotal(total, numberOfFriends) {
     const sumDivided = total / numberOfFriends;
     return sumDivided;
 }
-//divideTotal(total, numberOfFriends);
-const friendSum = divideTotal(total, numberOfFriends);
 function showDividedSum(sum) {
     console.log(sum);
     document.getElementById("showSum").classList.toggle("hide");
@@ -23,7 +15,11 @@ function showDividedSum(sum) {
 document
     .getElementById("calculateButton")
     .addEventListener("click", function () {
-    calculateTip(sum, tip);
-    divideTotal(total, numberOfFriends);
+    const tip = parseFloat(document.getElementById("tip").value);
+    const sum = parseFloat(document.getElementById("sum").value);
+    const numberOfFriends = parseFloat(document.getElementById("numberOfFriends").value);
+    const calculatedTip = calculateTip(sum, tip);
+    const total = sum + calculatedTip;
+    const friendSum = divideTotal(total, numberOfFriends);
     showDividedSum(friendSum);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,36 @@
-const tip: number = parseFloat(
-  (<HTMLInputElement>document.getElementById("tip")).value
-);
-const sum: number = parseFloat(
-  (<HTMLInputElement>document.getElementById("sum")).value
-);
-
-const numberOfFriends: number = parseFloat(
-  (<HTMLInputElement>document.getElementById("numberOfFriends")).value
-);
-
 function calculateTip(sum: number, tip: number) {
   const total = sum * tip;
 
   return total;
 }
-//calculateTip(sum, tip);
-const calculatedTip: number = calculateTip(sum, tip);
-const total = sum + calculatedTip;
 
 function divideTotal(total: number, numberOfFriends: number) {
   const sumDivided = total / numberOfFriends;
   return sumDivided;
 }
-//divideTotal(total, numberOfFriends);
-const friendSum = divideTotal(total, numberOfFriends);
-
 function showDividedSum(sum: number) {
   console.log(sum);
   document.getElementById("showSum").classList.toggle("hide");
   document.getElementById("inputForm").classList.toggle("hide");
   document.getElementById("friendSum").innerHTML = sum + " kr";
 }
+
 document
   .getElementById("calculateButton")
   .addEventListener("click", function () {
-    calculateTip(sum, tip);
-    divideTotal(total, numberOfFriends);
+    const tip: number = parseFloat(
+      (<HTMLInputElement>document.getElementById("tip")).value
+    );
+    const sum: number = parseFloat(
+      (<HTMLInputElement>document.getElementById("sum")).value
+    );
+
+    const numberOfFriends: number = parseFloat(
+      (<HTMLInputElement>document.getElementById("numberOfFriends")).value
+    );
+
+    const calculatedTip: number = calculateTip(sum, tip);
+    const total = sum + calculatedTip;
+    const friendSum = divideTotal(total, numberOfFriends);
     showDividedSum(friendSum);
   });
